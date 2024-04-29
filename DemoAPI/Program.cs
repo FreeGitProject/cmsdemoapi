be0d3b1f6d40ed9314dea6f92e5aacf7845271b7
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2024-04-29 19:07:26 +0530

    add copy to clipboard
    add copy to clipboard

--- DemoAPI/Program.cs
+++ DemoAPI/Program.cs
@@ -51,6 +51,7 @@ builder.Services.AddSwaggerGen(c =>
         Type = SecuritySchemeType.ApiKey,
         Scheme = "Bearer"
     });
+    //lock
     //c.AddSecurityRequirement(new OpenApiSecurityRequirement
     //{
     //    {
